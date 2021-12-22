e75c0db1fec66faadaa5715898d8dce9f06497b8
Daniil <barkovdc@gmail.com>
2021-12-22 16:40:38 +0100

    Legal and Privacy centered

--- components/global/MainContent.vue
+++ components/global/MainContent.vue
@@ -28,9 +28,15 @@
         </div>
       </div>
     </div>
-    <div class="bg-white max-w-7xl mx-auto sm:px-6 lg:px-8">
-      <slot/>
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+      <div class="max-w-3xl mx-auto">
+        <slot />
+      </div>
     </div>
+
+
   </div>
 </template>
 
