2ea65f5a93e1bccfe1abd80f97b5adf8254247ea
Harun Samardzic <harun.samardzic@redlink.at>
2021-12-22 16:31:06 +0100

    Add microsoft bookings

--- components/global/MainContent.vue
+++ components/global/MainContent.vue
@@ -19,7 +19,7 @@
             </p>
             <div v-if="siteImageAction" class="mt-10 max-w-sm mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div class="space-y-4 sm:space-y-0 sm:mx-auto sm:gap-5">
-                <a href="#" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-neutral-400 bg-opacity-60 hover:bg-opacity-70 sm:px-8">
+                <a v-bind:href="siteImageActionUrl" class="flex items-center justify-center px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-neutral-400 bg-opacity-60 hover:bg-opacity-70 sm:px-8">
                   {{ siteImageAction }}
                 </a>
               </div>
@@ -53,6 +53,10 @@ export default {
     siteImageAction: {
       type: String,
       default: ''
+    },
+    siteImageActionUrl: {
+      type: String,
+      default: '#'
     }
   }
 };
