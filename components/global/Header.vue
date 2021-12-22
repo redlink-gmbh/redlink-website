288fb77ac58dfdae5a14c160e587c4e342bf3efb
tkurz <thomas.kurz@redlink.at>
2021-12-22 13:56:31 +0100

    add header

--- components/global/Header.vue
+++ components/global/Header.vue
@@ -1,122 +1,100 @@
 <template>
-  <nav class="bg-white">
-    <div class="flex items-center justify-center">
-      <div class="absolute menu-button-container left-0 sm:hidden">
-        <!-- Mobile menu button-->
-        <button
-          @click="open = !open"
-          type="button"
-          class="inline-flex items-center justify-center p-2 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-          aria-controls="mobile-menu"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="block h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-          <svg
-            class="hidden h-6 w-6"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div class="flex items-center justify-center">
-        <div class="flex align-center items-center m-2">
-          <nuxt-link :to="localePath('/')" class="flex items-center">
-            <img class="block lg:hidden h-24 z-10 bg-white logo" src="/icon.png" alt="Workflow" />
-            <img class="hidden lg:block h-24 z-10 bg-white logo" src="/icon.png" alt="Workflow" />
-            <span class="hidden lg:block ml-3 font-black uppercase name-header">Weinmacher Nick</span>
-          </nuxt-link>
-        </div>
-        <div class="hidden sm:block sm:ml-6">
-          <div class="flex space-x-4">
-            <nuxt-link
-              @click="open = false"
-              class="px-3 rounded-b-none rounded-t-md py-2 text-sm font-medium header-link"
-              v-for="route of routes"
-              :key="route.keyName"
-              :to="localePath(route.route)"
-              >{{ $t(`header.${route.keyName}`) }}</nuxt-link
-            >
-          </div>
-        </div>
-      </div>
+  <div class="header" id="header">
+    <div>
+      <nuxt-link :to="localePath('/')">
+        <img class="logo" src="/logo-white.png" alt="Redlink GmbH" />
+      </nuxt-link>
     </div>
-
-    <transition name="fade">
-      <div v-if="open" class="sm:hidden" id="mobile-menu">
-        <div class="px-2 pt-2 pb-3 space-y-1">
-          <nuxt-link
-            @click.native="open = false"
-            class="block rounded-b-none rounded-t-md px-3 py-2 text-base font-medium header-link"
-            v-for="route of routes"
-            :key="route.keyName"
-            :to="localePath(route.route)"
-            >{{ $t(`header.${route.keyName}`) }}</nuxt-link
-          >
-        </div>
-      </div>
-    </transition>
-  </nav>
+    <div class="links">
+      <nuxt-link
+          class="link"
+          @click="open = false"
+          v-for="route of routes"
+          :key="route.keyName"
+          :to="localePath(route.route)"
+      >{{ $t(`header.${route.keyName}`) }}</nuxt-link>
+    </div>
+  </div>
 </template>
 
 <script>
+
 export default {
   name: 'Header',
   data: () => {
     return {
       open: false,
       routes: [
-        { keyName: 'aboutUs', route: '/about' },
-        { keyName: 'grapeVarieties', route: '/varieties' },
-        { keyName: 'news', route: '/news' },
-        { keyName: 'contact', route: '/contact' }
+        { keyName: 'about', route: '/' },
+        { keyName: 'team', route: '/team' },
+        { keyName: 'jobs', route: '/jobs' },
+        { keyName: 'blog', route: '/blog' }
       ]
     };
+  },
+  mounted:() => {
+    var scrollpos = window.scrollY;
+    var header = document.getElementById("header");
+
+    function add_class_on_scroll() {
+      header.classList.add("header-fade-in");
+    }
+
+    function remove_class_on_scroll() {
+      header.classList.remove("header-fade-in");
+    }
+
+    window.addEventListener('scroll', function(){
+      //Here you forgot to update the value
+      scrollpos = window.scrollY;
+
+      if(scrollpos > 20){
+        add_class_on_scroll();
+      }
+      else {
+        remove_class_on_scroll();
+      }
+    });
   }
 };
 </script>
 
 <style lang="postcss" scoped>
+.header {
+  width: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  height: 80px;
+  background-color: black;
+  padding: 0 50px;
+  transition: background-color 0.5s ease;
+}
+.header-fade-in {
+  background-color: red;
+  transition: background-color 0.5s ease;
+}
 .logo {
-  border-radius: 6rem;
-  opacity: 0.9;
-  box-shadow: 0 -1px 2px var(--color-primary-200);
+  height: 60px;
+  margin-top: 10px;
 }
-.menu-button-container {
-  top: 1.2rem;
+
+.links {
+  padding-top: 30px;
 }
-#mobile-menu {
-  margin-top: -35px;
-  margin-bottom: 35px;
-}
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
-.header-link {
-  margin-bottom: -5px;
+
+.link {
+  font-weight: bold;
+  color: white;
+  padding: 5px 10px;
+  border-radius: 20px;
+  height: 40px;
+  border: 2px solid white;
+  margin-left: 10px;
+
   &:hover {
-    @apply bg-primary-200 text-white;
-  }
-}
-.name-header {
-  margin-bottom: -5px;
+    background-color: white;
+    color: black;
+   }
 }
 </style>
