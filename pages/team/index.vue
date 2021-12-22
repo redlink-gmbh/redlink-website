1553672f8c977bacc4440dc0260981a7f0523dd3
Daniil <barkovdc@gmail.com>
2021-12-22 16:55:52 +0100

    Team header deleted

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -1,11 +1,11 @@
 <template>
   <main-content>
     <div class="bg-white">
-      <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+      <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-12">
         <div class="space-y-12">
           <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
-            <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl">Our Team</h2>
-            <p class="text-xl text-gray-500">Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus quis et.</p>
+            <h2 class="text-3xl font-extrabold tracking-tight sm:text-4xl"></h2>
+            <p class="text-xl text-gray-500"></p>
           </div>
           <ul role="list" class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
             <li v-for="person in team" :key="person.name">
