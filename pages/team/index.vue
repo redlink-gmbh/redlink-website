fb2767979b67830e3f634cc5b1d20d643c149ca5
markusebner <markus.ebner@redlink.at>
2022-02-11 11:35:42 +0100

    better overlay

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -12,8 +12,14 @@
               <div class="space-y-4">
                 <div class="employee-image aspect-w-3 aspect-h-2 relative">
                   <img class="object-cover shadow-lg rounded-lg" :src="person.imageUrl" alt="" />
-                  <div class="overlay-cite hidden absolute w-full h-full top-0 text-center">
-                    <span>Frontend is my passion!</span>
+                  <div class="overlay-cite hidden absolute w-full h-full top-0 flex items-center justify-center text-white">
+                    <svg class="absolute transform -translate-x-24 -translate-y-6 h-8 w-8 text-white opacity-25 z-0" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+                      <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                    </svg>
+                    <blockquote class="italic z-10 font-bold">Frontend is my passion!</blockquote>
+                    <svg class="absolute transform translate-x-24 translate-y-6 rotate-180 h-8 w-8 text-white opacity-25 z-0" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
+                      <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
+                    </svg>
                   </div>
                 </div>
 
@@ -74,11 +80,9 @@ export default {
 </script>
 <style lang="postcss" scoped>
   .overlay-cite {
-    display: none;
-    background: rgba(0,0,0, 0.6);
-    color: white;
+    background: rgba(0,0,0, 0.7);
   }
   .employee-image:hover .overlay-cite{
-    display: block;
+    display: flex;
   }
 </style>
